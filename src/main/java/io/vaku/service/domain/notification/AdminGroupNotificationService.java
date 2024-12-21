a903808b0ed34370d1de0684cc5def4381fd6807
Vadim Kurnyshov <vaku878@gmail.com>
2024-12-21 15:05:08 +0400

    fix

--- src/main/java/io/vaku/service/domain/notification/AdminGroupNotificationService.java
+++ src/main/java/io/vaku/service/domain/notification/AdminGroupNotificationService.java
@@ -1,16 +1,25 @@
 package io.vaku.service.domain.notification;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminGroupNotificationService {
-    private final static long ADMIN_CHAT_ID = -1002458275274L;
+    private final long adminChatId;
+    private final NotificationService notificationService;
 
     @Autowired
-    private NotificationService notificationService;
+    public AdminGroupNotificationService(
+            long adminChatId,
+            @Value("${app.feature.notifications.admin.group-id}")
+            NotificationService notificationService
+    ) {
+        this.adminChatId = adminChatId;
+        this.notificationService = notificationService;
+    }
 
     public void sendMessage(String msg) {
-        notificationService.notify(ADMIN_CHAT_ID, msg);
+        notificationService.notify(adminChatId, msg);
     }
 }
