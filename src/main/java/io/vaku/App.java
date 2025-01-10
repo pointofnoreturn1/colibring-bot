ef1a1c490183d9930d107b492b794c42159f8970
Vadim Kurnyshov <vaku878@gmail.com>
2025-01-10 22:16:20 +0400

    fix

--- src/main/java/io/vaku/App.java
+++ src/main/java/io/vaku/App.java
@@ -3,11 +3,13 @@ package io.vaku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
+@PropertySource("classpath:application.yml")
 public class App {
 
     public static void main(String[] args) {
