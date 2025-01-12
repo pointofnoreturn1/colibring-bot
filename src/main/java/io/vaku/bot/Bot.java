c55d6164a6be1ac5d9b6fec18de20edb569efe74
Vadim Kurnyshov <vaku878@gmail.com>
2025-01-12 17:33:11 +0400

    resolve feature/cb-26

--- src/main/java/io/vaku/bot/Bot.java
+++ src/main/java/io/vaku/bot/Bot.java
@@ -5,6 +5,7 @@ import io.vaku.model.Response;
 import io.vaku.model.domain.User;
 import io.vaku.service.UpdateHandlerService;
 import io.vaku.service.domain.UserService;
+import io.vaku.service.notification.AdminNotificationService;
 import io.vaku.util.EnvHolder;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
@@ -27,14 +28,21 @@ public class Bot extends TelegramLongPollingBot {
 
     private final UpdateHandlerService updateHandlerService;
     private final UserService userService;
+    private final AdminNotificationService adminNotificationService;
     private final String botName;
     private final int timeout;
 
     @Autowired
-    public Bot(UpdateHandlerService updateHandlerService, UserService userService, EnvHolder envHolder) {
+    public Bot(
+            UpdateHandlerService updateHandlerService,
+            UserService userService,
+            AdminNotificationService adminNotificationService,
+            EnvHolder envHolder
+    ) {
         super(System.getenv("BOT_TOKEN"));
         this.updateHandlerService = updateHandlerService;
         this.userService = userService;
+        this.adminNotificationService = adminNotificationService;
         this.botName = envHolder.getBotName();
         this.timeout = envHolder.getBotConnectionTimeout();
     }
@@ -43,7 +51,9 @@ public class Bot extends TelegramLongPollingBot {
     private void init() {
         try {
             new TelegramBotsApi(DefaultBotSession.class).registerBot(this);
-            log.info("TelegramAPI started. Looking for messages");
+            var msg = "Telegram API started. Looking for messages";
+            log.info(msg);
+            adminNotificationService.sendMessage(msg);
         } catch (TelegramApiRequestException e) {
             log.info("Unable to connect. Pause {} seconds and try again. Error: {}", timeout / 1000, e.getMessage());
             try {
